da8bf2ba373442c2fc7aadab1edd02d8f1da3d33
202407030LHH <super8114@yuhan.ac.kr>
2025-06-19 14:54:22 +0900

    4. main [ * ] calc.cpp test after modified

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -15,13 +15,12 @@ int main()
 	int y = 0;
 	int i = 0;
 
-		std::cout << "첫번째 수를 입력하세요 : ";
-		std::cin >> x;
-		std::cout << "두번째 수를 입력하세요 : ";
-		std::cin >> y;
+	std::cout << "첫번째 수를 입력하세요 : ";
+	std::cin >> x;
+	std::cout << "두번째 수를 입력하세요 : ";
+	std::cin >> y;
 
-		printf("입력된 수 x[ %d ] y[ %d ]\n", x, y);
-		printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n",
-			add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
-	}
+	printf("입력된 수 x[ %d ] y[ %d ]\n", x, y);
+	printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n",
+		add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
 }
